6d91a21613daba2caefcfd413efc334b12d74a7a
shahadat <shahadat.hossan@bgc-bd.com>
2026-02-01 15:44:17 +0600

    add COUNTER_STATE on counter-selector

--- src/app/counter/states/counter-selector.ts
+++ src/app/counter/states/counter-selector.ts
@@ -1,6 +1,7 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { CounterState } from "./counter-state";
+import { COUNTER_STATE } from "src/app/constants";
 
-const getcounterState=createFeatureSelector<CounterState>('counter');
+const getcounterState=createFeatureSelector<CounterState>(COUNTER_STATE);
 export const getcounter=createSelector(getcounterState,(state)=>state.counter);
 export const getToggle=createSelector(getcounterState,(state)=>state.toggle);
