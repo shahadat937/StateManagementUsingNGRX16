48d6b013e451af3bbfee0de0de50584581d49d68
shahadat <shahadat.hossan@bgc-bd.com>
2026-02-09 12:28:28 +0600

    remove course action previous code

--- src/app/courses/state/courses.action.ts
+++ src/app/courses/state/courses.action.ts
@@ -3,32 +3,3 @@ import { createAction, props } from '@ngrx/store';
 import { Course } from 'src/app/models/course.model';
 
 export const showForm = createAction('showForm', props<{ value: boolean }>());
-export const createCourse = createAction(
-  'createCourse',
-  props<{ course: Course }>(),
-);
-export const createCourseSuccess = createAction(
-  '[courses] create course success',
-  props<{ course: Course }>(),
-);
-
-export const readCourses = createAction('[courses] read courses');
-export const readCoursesSuccess = createAction(
-  '[courses] read courses success',
-  props<{ courses: Course[] }>(),
-);
-export const updateCourse = createAction(
-  'updateCourse',
-  props<{ course: Course }>(),
-);
-export const updateCourseSuccess = createAction(
-  '[courses] update course success',
-  props<{ course: Update<Course>}>()
-);
-export const deleteCourse = createAction(
-  'deleteCourse',
-  props<{ id: string }>(),
-);
-export const deleteCourseSuccess = createAction('[courses] delete course success', 
-    props<{ id: string}>()
-)
