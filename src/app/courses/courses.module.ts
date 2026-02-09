0f4fe766a3644ccce2dd73d94b0f327243432671
shahadat <shahadat.hossan@bgc-bd.com>
2026-02-09 12:37:22 +0600

    CoursesEffect reference remove from course module

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -9,13 +9,15 @@ import { StoreModule } from "@ngrx/store";
 import { COURSES_STATE } from "../constants";
 import { coursesReducer } from "./state/courses.reducer";
 import { EffectsModule } from "@ngrx/effects";
-import { CoursesEffect } from "./state/courses.effects";
 import { authGuard } from "../auth/services/auth.guard";
 import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { courseDetailResolver } from "../resolvers/course-detail.resolver";
 
 const routes: Routes = [
   { path: '', component: CoursesComponent,canActivate: [authGuard], } , // Shows CoursesComponent at /courses
-  { path: 'course/:id', component: CourseDetailComponent,canActivate: [authGuard], }  
+  { path: 'course/:id', component: CourseDetailComponent,canActivate: [authGuard], resolve:{
+    courseResolver:courseDetailResolver
+  }}  
 ];
 
 @NgModule({
@@ -29,7 +31,7 @@ const routes: Routes = [
     imports:[
        CommonModule,
     ReactiveFormsModule,
-      EffectsModule.forFeature([CoursesEffect]),
+      EffectsModule.forFeature(),
     RouterModule.forChild(routes),
     StoreModule.forFeature(COURSES_STATE,coursesReducer)
     ],
